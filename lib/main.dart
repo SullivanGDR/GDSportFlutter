0878ad73814f7b3dc8405749383bdc2074e51ad6
SullivanGDR <sullivan04gendre@gmail.com>
2023-12-06 10:55:58 +0100

    Projet structuré

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:carousel_slider/carousel_slider.dart';
+import 'package:gdsport_flutter/homepage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,93 +17,6 @@ class MyApp extends StatelessWidget {
       ),
       home: const MyHomePage(title: 'GDSport'),
       debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        centerTitle: true,
-        title: Text(widget.title, style: TextStyle(fontWeight: FontWeight.bold)),
-        leading: Icon(Icons.menu),
-        actions: const <Widget> [
-          Padding(padding: EdgeInsets.only(right: 15), child: Icon(Icons.search)),
-        ],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            CarouselSlider(
-              items: [
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  color: Colors.black87,
-                  child: Center(
-                    child: Text(
-                      'SOLDES DERNIERES DEMARQUES',
-                      style: TextStyle(fontSize: 18.0, color: Colors.white),
-                    ),
-                  ),
-                ),
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  color: Colors.black87,
-                  child: Center(
-                    child: Text(
-                      'RESTOCK DES AF1',
-                      style: TextStyle(fontSize: 18.0, color: Colors.white),
-                    ),
-                  ),
-                ),
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  color: Colors.black87,
-                  child: Center(
-                    child: Text(
-                      'OFFRES ETUDIANTES',
-                      style: TextStyle(fontSize: 18.0, color: Colors.white),
-                    ),
-                  ),
-                ),
-              ],
-              options: CarouselOptions(
-                height: 50,
-                autoPlay: true,
-                viewportFraction: 1,
-                autoPlayInterval: Duration(seconds: 3),
-                autoPlayAnimationDuration: Duration(milliseconds: 800),
-                autoPlayCurve: Curves.fastOutSlowIn,
-                enableInfiniteScroll: true,
-              ),
-            )
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Panier',
-        backgroundColor: Colors.white,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15), // Rayon des coins
-        ),
-        elevation: 10,
-        child: const Icon(Icons.shopping_bag_outlined),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
