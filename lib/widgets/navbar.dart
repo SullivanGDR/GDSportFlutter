5c708f8b8f64e377cc8e0610620d2019c8198b15
SullivanGDR <sullivan04gendre@gmail.com>
2023-12-28 13:34:26 +0100

    Ajout de classe Article, des fonctions des appels API, carousel des tendances

---
+++ lib/widgets/navbar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+PreferredSizeWidget appBar(BuildContext context) {
+  return AppBar(
+    backgroundColor: Colors.white,
+    centerTitle: true,
+    title: const Text('GDSport',
+        style: TextStyle(fontWeight: FontWeight.bold)),
+    leading: const Icon(Icons.menu),
+    actions: const <Widget>[
+      Padding(
+          padding: EdgeInsets.only(right: 15), child: Icon(Icons.search)),
+    ],
+  );
+}
